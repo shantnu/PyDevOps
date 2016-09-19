4be3193392386b61abaa3ec28a32ccf29c8013f4
Shantnu Tiwari <shantnu@users.noreply.github.com>
2016-09-19 12:21:56 +0100

    run_cmd, read file, get/put file working

--- pydevops/pydevops.py
+++ pydevops/pydevops.py
@@ -10,6 +10,7 @@ class Devops():
             self.ssh.connect(server, username=username, password=password)
         except:
             raise Exception("Failed to connect")
+        self.sftp = self.ssh.open_sftp()
 
     def __del__(self):
         self.ssh.close()
@@ -20,13 +21,43 @@ class Devops():
     def __exit__(self,exc_type, exc_val, exc_tb):
         print("closing ssh")
         self.__del__()
+
+    def run_command(self, cmd):
+        stdin, stdout, stderr = self.ssh.exec_command(cmd)
+
+        out = stdout.read()
+        err = stderr.read()
+
+        return out, err
         
 
+    def copy_file_to_remote(self, filename, remote_path):
+        self.sftp.put(filename, remote_path)
 
+    def copy_file_from_remote(self, remote_path, local_path):       
+        self.sftp.get(remote_path, local_path)
+
+
+    def read_remote_file(self, filename):
+        remote_file = self.sftp.open(filename)
+        data = remote_file.read()
+        return data
 
 if __name__ == '__main__':
     with Devops("192.168.33.10", "vagrant", "vagrant") as dvp:
+        dvp.copy_file_to_remote("./meow.txt", "/vagrant/meow.txt")
+        
+        print("\n\n reading remote file: ",dvp.read_remote_file("/vagrant/meow.txt"), "\n\n")
+
+        dvp.copy_file_from_remote("/vagrant/meow.txt", "./meow2.txt")
+
+
+        out,err = dvp.run_command("cd /vagrant; cat d.py")
+        print("\n", out, "\n", err, "\n")
         print("Yo!")
+
+        out,err = dvp.run_command("pasta luv!")
+        print("\n", out, "\n", err, "\n")
 
             
         
