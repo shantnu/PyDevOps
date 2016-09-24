cb5342ce2658f4f2a4cb4b7e994da3c3cde15080
Shantnu Tiwari <shantnu@users.noreply.github.com>
2016-09-24 13:16:54 +0100

    1st version working

--- pydevops/controller.py
+++ pydevops/controller.py
@@ -1,6 +1,7 @@
 import strictyaml
 from pydevops import Devops
 import os
+import pdb
 
 def read_yaml(yaml_file):
      with open(yaml_file) as f:
@@ -16,7 +17,7 @@ def main():
             break
 
     yaml = read_yaml(yaml_file)            
-    print(yaml)
+    #print(yaml)
 
     if yaml['python_ver'] != "python3":
         print("Only python 3 supported.")
@@ -26,14 +27,17 @@ def main():
     # convert list to string with spaces
     for a in yaml['apt-get installs']:
         apt_installs += " " + a
-    print(apt_installs)
+    #print(apt_installs)
 
-    requirements_file = yaml['requirements file']
+    requirements_file =  yaml['requirements file']
 
     username = "vagrant"
-    with Devops("192.168.33.10", "vagrant", "vagrant") as dvp:
+    home = "/home/" + username + "/"
+    server = yaml['server']
+
+    with Devops(server, username, "vagrant") as dvp:
         with open("devops.log", "wb") as log:
-            '''
+
             out,err = dvp.run_command("sudo apt-get update")
             log.write(out+err)
 
@@ -43,21 +47,29 @@ def main():
             cmd = "sudo apt-get install " + apt_installs + " -y"
             out,err = dvp.run_command(cmd)
             log.write(out+err)
-            
 
-            remote_loc = "~/" + requirements_file
+            
+            remote_loc = home + requirements_file
             dvp.copy_file_to_remote(requirements_file, remote_loc)
 
-            remote_loc = "~/" + "install_python.sh"
+            remote_loc = home + "install_python.sh"
             dvp.copy_file_to_remote("install_python.sh", remote_loc)
 
             cmd = "cd ~;chmod +x install_python.sh; ./install_python.sh"
             out,err = dvp.run_command(cmd)
             log.write(out+err)
-            '''
+            
 
-            # cmd = "cd ~; pyvenv myenv; ~/myenv/bin/activate; pip install -r ~/" + requirements_file
-            cmd = "which python; source ~/.bashrc; which python"
+            cmd = "~/miniconda3/bin/conda create --name " + yaml['env_name'] + " python=3 -y"
+            out,err = dvp.run_command(cmd)
+            log.write(out+err)
+
+
+            remote_loc = "/home/vagrant/" + requirements_file
+            dvp.copy_file_to_remote("./" +requirements_file, remote_loc)
+
+            cmd = "~/miniconda3/envs/" + yaml['env_name']+"/bin/pip install -r ~/" + requirements_file
+            print(cmd)
             out,err = dvp.run_command(cmd)
             log.write(out+err)
 
